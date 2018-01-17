a456302bac503b1cb4f9fc9eace5ada95f9421dd
Eli Cleveland <elicleveland12@gmail.com>
2018-01-17 20:48:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var recipe = { ice: "1 large cube", whiskey: "2 parts", vermouth: "1 part", cherries: "2 or 3", orange: "1/3 peel" };
+var recipes = { ice: "1 large cube", whiskey: "2 parts", vermouth: "1 part", bitters: "2 dashes", cherries: "2 or 3", orange: "1/3 peel" };
