b19adbed681984a7f271d1e73ec0212abd4a1ede
Eli Cleveland <elicleveland12@gmail.com>
2018-01-17 21:13:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var simpleRecipe = Object.assign({}, recipe);
-  delete object[key];
-  return object;
+  delete simpleRecipe[key];
+  return simpleRecipe;
 }
