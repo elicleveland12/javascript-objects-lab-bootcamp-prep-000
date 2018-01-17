a57bc2c085ec2f7826fb04dd43668e6944d8e7cb
Eli Cleveland <elicleveland12@gmail.com>
2018-01-17 20:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = { ice: "1 large cube", whiskey: "2 parts", vermouth: "1 part", bitters: "2 dashes", cherries: "2 or 3", orange: "1/3 peel" };
+var recipes = { ice: "1 large cube", whiskey: "2 parts", vermouth: "1 part", bitters: "2 dashes", cherries: "2 or 3" };
 
 function updateObjectWithKeyAndValue(object, key, value) {
   
